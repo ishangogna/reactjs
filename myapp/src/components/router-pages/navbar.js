bd62b94da80fd49720d8b96133c6943de6810ebc
Ishan Gogna <ishangogna10@gmail.com>
2020-06-29 08:53:16 +0530

    Link/NavLink

--- myapp/src/components/router-pages/navbar.js
+++ myapp/src/components/router-pages/navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Link, NavLink} from 'react-router-dom';
 
+//importing navlink and link from react-router-dom for react handling of requests
+// so theat they do dont go to server each time. 
+//react intercepts a url request and avoids making unecessary request to server.
 
+//Note : Both Link and NavLink work in the same way. They can be used in place of a tags
+// to prevent default behaviour of making request to server and reloading page.
+//But NavLink also assigns an active class to the current component (helpful in making navbars)
 const Navbar = () => {
     return (
         <nav className = 'nav-wrapper red darken-3'>
             <div className = 'container'>
                 <a className = 'brand-logo'>My React Website.</a>
                 <ul className = 'right'>
-                    <li><a href = '/'>Home</a></li>
-                    <li><a href = '/about'>About Us</a></li>
-                    <li><a href = '/contact'>Contact us</a></li>
+                    <li><Link to= '/'>Home</Link></li>
+                    <li><NavLink to = '/about'>About Us</NavLink></li>
+                    <li><Link to = '/contact'>Contact us</Link></li>
                 </ul>
             </div>
         </nav>
